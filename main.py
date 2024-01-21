9e7032a6dd60ab984c068716f54a4e55dc1f2bdb
Mikhail_Cherevkov <mikhailcherevkov@gmail.com>
2024-01-21 19:38:23 +0700

    requirements

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from numbers import B
-a = 2 + 2
-print(a+B)
+import numpy as np
+import scipy as sp
+
+mat1 = np.array([[1, 2], [3, 4]])
+mat2 = np.array([[1, 3], [7, 5]])
+mat3 = np.dot(mat1, mat2)
+det = sp.linalg.det(mat3)
+print("Определитель матрицы равен {:.2f}".format(det))
